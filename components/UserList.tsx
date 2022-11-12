5d95e2d7713d8153534830f147cf2a5a2ff4177d
fahreziadh <fahreziadh@gmail.com>
2022-11-12 16:17:59 +0700

    change query result

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -5,7 +5,7 @@ import UserItem from './UserItem'
 export default function UserList({result}:Props) {
   return (
     <div className='border rounded-[8px] w-full space-y-3 pb-10'>
-        <p className='text-center p-[20px]'>Hasil Pencarian : <span className='font-bold'>Budi</span></p>
+        <p className='text-center p-[20px]'>Hasil Pencarian : <span className='font-bold'>{result?.search}</span></p>
         {result?.users.map((user,index)=>(
           <UserItem key={index} user={user}/>
         ))}
